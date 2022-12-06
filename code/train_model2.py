3f5b8eab873d9b32d1d7036010cdd92a731624db
任奇 <87684714+renqi1@users.noreply.github.com>
2022-12-06 17:09:27 +0800

    Update train_model2.py

--- code/train_model2.py
+++ code/train_model2.py
@@ -7,11 +7,10 @@ from transformers import BertTokenizer
 from dataset import read_train_set, BuildDataSet2, convert_examples2
 from utils import get_columns
 
-
 train_set_path = "../data/waic_nl2sql_train.jsonl"
-pretain_vocab_path = "../pretain_model/ernie/vocab.txt"
-pretain_config_path = "../pretain_model/ernie/bert_config.json"
-pretain_model_path = "../pretain_model/ernie/pytorch_model.bin"
+pretrain_vocab_path = "../pretrain_model/ernie/vocab.txt"
+pretrain_config_path = "../pretrain_model/ernie/bert_config.json"
+pretrain_model_path = "../pretrain_model/ernie/pytorch_model.bin"
 model2_save_path = "../my_model/model2_ernie.pkl"
 hidden_size = 768
 batch_size = 32
@@ -21,19 +20,19 @@ columns = get_columns()
 train_examples = read_train_set(train_set_path)
 print('train examples got')
 
-tokenizer = BertTokenizer.from_pretrained(pretain_vocab_path)
+tokenizer = BertTokenizer.from_pretrained(pretrain_vocab_path)
 
 features = convert_examples2(train_examples, columns, tokenizer, max_length=128, train=True)
 train_dataset = BuildDataSet2(features)
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-print('dataloder finish')
+print('load data finish')
 
-model2 = Bert2(hidden_size=hidden_size, config=pretain_config_path, model=pretain_model_path).cuda()
+model2 = Bert2(hidden_size=hidden_size, config=pretrain_config_path, model=pretrain_model_path).cuda()
 
 loss_function = torch.nn.CrossEntropyLoss()
 optimizer = optim.Adam(model2.parameters(), lr=learning_rate)
-labels_all=[]
-predict_all=[]
+labels_all = []
+predict_all = []
 model2.train()
 for epoch in range(10):
     print('epoch', epoch)
@@ -50,16 +49,15 @@ for epoch in range(10):
         out_conds_value = out_conds_value.transpose(1, 2).reshape(-1, 63)
         loss_cond_value = loss_function(out_conds_value, conds_value)
 
-        total_loss = 5*loss_cond_value + loss_conn
+        total_loss = 5 * loss_cond_value + loss_conn
 
         optimizer.zero_grad()
         total_loss.backward()
         optimizer.step()
 
-        if (epoch_batch+1) % 20 == 0:
-            print(epoch_batch+1, '/', len(train_loader), '\t', loss_cond_value.item(), '\t', loss_conn.item())
+        if (epoch_batch + 1) % 20 == 0:
+            print(epoch_batch + 1, '/', len(train_loader), '\t', loss_cond_value.item(), '\t', loss_conn.item())
 
-        if (epoch_batch+1) % 100 == 0:
+        if (epoch_batch + 1) % 100 == 0:
             torch.save(model2.state_dict(), model2_save_path)
             print('model saved')
-
